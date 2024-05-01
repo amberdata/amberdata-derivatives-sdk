5100488ebf0aeaa8f7526ef054895cf79183bdc2
gravitysucks <105658134+gravity5ucks@users.noreply.github.com>
2024-05-01 17:11:32 +0200

    First (#1)
    Co-authored-by: GravitySucks <fabiobassani@MacBook-Air-di-Fabio.local>

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amberdata-derivatives',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Python client for Amberdata API for derivatives analytics.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Amberdata',
+    url='https://github.com/amberdata/amberdata-derivatives-sdk',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
