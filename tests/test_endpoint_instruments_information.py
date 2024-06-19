e284b164d27599604c9e691329ac3b3d3149a5fc
Joanes Espanol <joanes@amberdata.io>
2024-06-19 00:30:46 -0700

    DER-0000: re-factored test functions

--- tests/test_endpoint_instruments_information.py
+++ tests/test_endpoint_instruments_information.py
@@ -23,19 +23,19 @@ class EndpointInstrumentsInformationTestCase(BaseTestCase):
         self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
         self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
 
-    def test_exchange(self):
-        response = self.call_endpoint(exchange='deribit')
-        self.validate_response_schema(response, schema=self.schema)
-        self.validate_response_200(response)
-        self.validate_response_field(response, 'exchange', 'deribit')
-        self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
-        self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
-
     def test_currency(self):
         response = self.call_endpoint(currency='ETH')
         self.validate_response_schema(response, schema=self.schema)
         self.validate_response_200(response)
         self.validate_response_field(response, 'currency', 'ETH')
+        self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
+        self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
+
+    def test_exchange(self):
+        response = self.call_endpoint(exchange='deribit')
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response)
+        self.validate_response_field(response, 'exchange', 'deribit')
         self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
         self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
 
@@ -55,21 +55,21 @@ class EndpointInstrumentsInformationTestCase(BaseTestCase):
         self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
         self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
 
-    def test_timestamp(self):
+    def test_historical_timeformat_default(self):
         response = self.call_endpoint(timestamp='2024-04-01T03:00:00')
         self.validate_response_data(response)
         self.validate_response_200(response, num_elements=2952)
         self.validate_response_field_timestamp(response, 'endDate', is_milliseconds=True)
         self.validate_response_field_timestamp(response, 'expiration', is_milliseconds=True)
 
-    def test_timestamp_timeformat_hr(self):
+    def test_historical_timeformat_hr(self):
         response = self.call_endpoint(timestamp='2024-04-01T03:00:00', timeFormat='hr')
         self.validate_response_data(response)
         self.validate_response_200(response, num_elements=2952)
         self.validate_response_field_timestamp(response, 'endDate', is_hr=True)
         self.validate_response_field_timestamp(response, 'expiration', is_hr=True)
 
-    def test_timestamp_timeformat_iso(self):
+    def test_historical_timeformat_iso(self):
         response = self.call_endpoint(timestamp='2024-04-01T03:00:00', timeFormat='iso')
         self.validate_response_data(response)
         self.validate_response_200(response, num_elements=2952)
@@ -95,15 +95,15 @@ class EndpointInstrumentsInformationTestCase(BaseTestCase):
         self.validate_response_data(response)
         self.validate_response_200(response, num_elements=0)
 
-    def test_invalid_timestamp(self):
-        response = self.call_endpoint(timestamp='<timestamp>')
-        self.validate_response_data(response)
-        self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER_TIMESTAMP)
-
     def test_invalid_timeformat(self):
         response = self.call_endpoint(timeFormat='<time_format>')
         self.validate_response_data(response)
         self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER_TIME_FORMAT)
+
+    def test_invalid_timestamp(self):
+        response = self.call_endpoint(timestamp='<timestamp>')
+        self.validate_response_data(response)
+        self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER_TIMESTAMP)
 
     def test_unknown_currency(self):
         response = self.call_endpoint(currency='<currency>')
