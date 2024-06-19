8f2057c11ae30a73edbcde95a8b74ad7fb4e0e02
gravitysucks <105658134+gravity5ucks@users.noreply.github.com>
2024-06-19 06:13:42 +0200

    fix case when time_format not passed causing error (#11)
    Co-authored-by: GravitySucks <fabiobassani@MacBook-Air-di-Fabio.local>
    Co-authored-by: Joanes Espanol <joanesespanol@users.noreply.github.com>

--- amberdata_derivatives/amberdata.py
+++ amberdata_derivatives/amberdata.py
@@ -926,7 +926,7 @@ class AmberdataDerivatives:
 
         # Add default parameters
         if self.__time_format is not None:
-            if query_params['timeFormat'] is None:
+            if 'timeFormat' not in query_params:
                 query_params['timeFormat'] = self.__time_format
 
         # Build query and URL
