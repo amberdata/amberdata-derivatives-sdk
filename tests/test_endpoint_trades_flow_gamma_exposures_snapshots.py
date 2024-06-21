352579a1454ec775ed7a5cfea1eb4d5ae7498962
Joanes Espanol <joanes@amberdata.io>
2024-06-20 23:09:47 -0700

    DER-0000: added unit tests

---
+++ tests/test_endpoint_trades_flow_gamma_exposures_snapshots.py
@@ -0,0 +1,96 @@
+# ======================================================================================================================
+
+# pylint: disable=line-too-long, missing-class-docstring, missing-function-docstring, missing-module-docstring
+
+import unittest
+
+from tests.base_test_case import BaseTestCase
+from tests.error_message import ErrorMessage
+
+
+# ======================================================================================================================
+
+class EndpointTradesFlowGammaExposuresSnapshotsTestCase(BaseTestCase):
+    def setUp(self, function_name='get_trades_flow_gamma_exposures_snapshots'):
+        super().setUp(function_name)
+
+    # ==================================================================================================================
+
+    def test_default(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC')
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response, min_elements=500)
+        self.validate_response_field(response, 'exchange', 'deribit')
+        self.validate_response_field(response, 'currency', 'BTC')
+        self.validate_response_field_timestamp(response, 'expirationTimestamp', is_milliseconds=True)
+        self.validate_response_field_timestamp(response, 'snapshotTimestamp', is_milliseconds=True)
+
+    def test_historical(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', startDate='2024-04-01T00:00:00', endDate='2024-04-01T01:00:00')
+        self.validate_response_data(response)
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response, num_elements=912)
+        self.validate_response_field(response, 'exchange', 'deribit')
+        self.validate_response_field(response, 'currency', 'BTC')
+        self.validate_response_field_timestamp(response, 'expirationTimestamp', is_milliseconds=True)
+        self.validate_response_field_timestamp(response, 'snapshotTimestamp', is_milliseconds=True)
+
+    def test_historical_timeformat_default(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', startDate='2024-04-01T00:00:00', endDate='2024-04-01T01:00:00')
+        self.validate_response_data(response)
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response, num_elements=912)
+        self.validate_response_field(response, 'exchange', 'deribit')
+        self.validate_response_field(response, 'currency', 'BTC')
+        self.validate_response_field_timestamp(response, 'expirationTimestamp', is_milliseconds=True)
+        self.validate_response_field_timestamp(response, 'snapshotTimestamp', is_milliseconds=True)
+
+    def test_historical_timeformat_hr(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', startDate='2024-04-01T00:00:00', endDate='2024-04-01T01:00:00', timeFormat='hr')
+        self.validate_response_data(response)
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response, num_elements=912)
+        self.validate_response_field(response, 'exchange', 'deribit')
+        self.validate_response_field(response, 'currency', 'BTC')
+        self.validate_response_field_timestamp(response, 'expirationTimestamp', is_hr=True)
+        self.validate_response_field_timestamp(response, 'snapshotTimestamp', is_hr=True)
+
+    def test_historical_timeformat_iso(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', startDate='2024-04-01T00:00:00', endDate='2024-04-01T01:00:00', timeFormat='iso')
+        self.validate_response_data(response)
+        self.validate_response_schema(response, schema=self.schema)
+        self.validate_response_200(response, num_elements=912)
+        self.validate_response_field(response, 'exchange', 'deribit')
+        self.validate_response_field(response, 'currency', 'BTC')
+        self.validate_response_field_timestamp(response, 'expirationTimestamp', is_iso=True)
+        self.validate_response_field_timestamp(response, 'snapshotTimestamp', is_iso=True)
+
+    # ==================================================================================================================
+
+    def test_invalid_parameter(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', invalid='parameter')
+        self.validate_response_data(response)
+        self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER)
+
+    def test_invalid_timestamp(self):
+        response = self.call_endpoint(exchange='deribit', currency='BTC', startDate='<timestamp>')
+        self.validate_response_data(response)
+        self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER_TIMESTAMP)
+
+    def test_unknown_exchange(self):
+        response = self.call_endpoint(exchange='<exchange>', currency='BTC')
+        self.validate_response_data(response)
+        self.validate_response_200(response, num_elements=0)
+
+    def test_unknown_currency(self):
+        response = self.call_endpoint(exchange='deribit', currency='<currency>')
+        self.validate_response_data(response)
+        self.validate_response_200(response, num_elements=0)
+
+
+# ======================================================================================================================
+
+if __name__ == '__main__':
+    unittest.main()
+
+# ======================================================================================================================
