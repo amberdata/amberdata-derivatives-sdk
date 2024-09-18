ff9db72c2d9ee49fa29261922700b2173149b2e7
Peter Yom <ypeter999@gmail.com>
2024-09-18 15:26:16 -0400

    remove gamma exposure exchange test

--- tests/test_endpoint_trades_flow_gamma_exposures_snapshots.py
+++ tests/test_endpoint_trades_flow_gamma_exposures_snapshots.py
@@ -88,11 +88,6 @@ class EndpointTradesFlowGammaExposuresSnapshotsTestCase(BaseTestCase):
         self.validate_response_data(response)
         self.validate_response_400(response, ErrorMessage.INVALID_PARAMETER_TIMESTAMP)
 
-    def test_unknown_exchange(self):
-        response = self.call_endpoint(exchange='<exchange>', currency='BTC')
-        self.validate_response_data(response)
-        self.validate_response_200(response, num_elements=0)
-
     def test_unknown_currency(self):
         response = self.call_endpoint(exchange='deribit', currency='<currency>')
         self.validate_response_data(response)
